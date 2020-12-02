135b4da09736b891bef39195bc11640e80caf019
Antonio <antonio.gm.ac@gmail.com>
2020-12-02 19:26:24 +0100

    Nuevos proyectos

---
+++ Ejemplo objetov 3D/Assets/Scripts/metodos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class metodos : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private int numero1 = 6;
+    private int numero2 = 4;
+
+    void Start()
+    {
+        //mostrarSuma();
+        Debug.Log(CalcularSuma(numero1, numero2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mostrarSuma();
+        Debug.Log(CalcularSuma(numero1, numero2));
+    }
+
+    //accion
+    void mostrarSuma()
+    {
+        Debug.Log("La suma de los numeros es: " + (this.numero1 + this.numero2));
+    }
+
+    //funcion
+    int CalcularSuma(int numero1, int numero2)
+    {
+        int suma = numero1 + numero2;
+        return suma;
+    }
+}
---
+++ Proyecto 1/Assets/Scripts/cube_move.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class cube_move : MonoBehaviour
+{
+    [SerializeField] private int numero = 1;
+    private float numero2;
+    private bool verdadero = true;
+    private string nombre = "Antonio";
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(numero);
+        Debug.Log("mi nombre es : " + nombre + "Gonzalez");
+
+        numero += 3; //numero = numero + 3;
+        numero -= 3; //numero = numero - 3;
+        numero++;
+        numero--;
+
+        Debug.Log(numero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(numero);
+        numero++;
+    }
+}
